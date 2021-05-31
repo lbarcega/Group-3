5b098b9e1ad317796edc8b10e36b346af2a3d531
crfirmalo <crfirmalo@student.apc.edu.ph>
2021-05-31 20:53:49 +0800

    Update header.php
    change custom css href

--- codeigniter/application/views/templates/header.php
+++ codeigniter/application/views/templates/header.php
@@ -8,7 +8,7 @@
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
 		<!-- Custom CSS -->
-		<link rel="stylesheet" type="text/css" href="\views\css\app.css">
+		<link rel="stylesheet" type="text/css" href="../assets/css/style.css">
                 <title>Time Off <?php echo $title; ?></title>
         </head>
         <body>
