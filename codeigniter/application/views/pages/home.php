600a97533785d91d700b9eccbef68f11eddf5c66
nicolepalenzuela <84000059+nicolepalenzuela@users.noreply.github.com>
2021-06-08 20:22:12 +0800

    Update home.php

--- codeigniter/application/views/pages/home.php
+++ codeigniter/application/views/pages/home.php
@@ -2,20 +2,20 @@
 			<div class="mx-auto">
 				<h1>Strategic Market Risk Consultancy Insurance Agency Inc.</h1>
 				<hr>
-						<a href="" class"btn btn-primary btn-lg home-button">Employee Information</a>
+						<a href="" class="btn btn-primary btn-lg home-button">Employee Information</a>
 						<br>
 					<div class="row">
 						<div class="col">
-								<a href="login" class"btn btn-primary btn-lg home-button">Sign in</a>
+								<a href="login" class="btn btn-primary btn-lg home-button">Sign in</a>
 						</div>
 							<div class="col">
-								<a href="" class"btn btn-primary btn-lg home-button">Sign out</a>
+								<a href="" class="btn btn-primary btn-lg home-button">Sign out</a>
 							</div>
 						</div>
 						<br>
-							<a href="" class"btn btn-primary btn-lg home-button">Apply for Leave</a>
+							<a href="" class="btn btn-primary btn-lg home-button">Apply for Leave</a>
 						<br>
-							<a href="" class"btn btn-primary btn-lg home-button">somethingsgjrnn</a>
+							<a href="" class="btn btn-primary btn-lg home-button">somethingsgjrnn</a>
 
 
 			</div>
