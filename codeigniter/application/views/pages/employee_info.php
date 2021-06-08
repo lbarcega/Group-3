4e0ac0f8b0953f60323f43fb4235659f87fc63b4
lbarcega <84000664+lbarcega@users.noreply.github.com>
2021-06-08 18:26:18 +0800

    Update employee_info.php
    wrap last input in <div>
    add missing </div>

--- codeigniter/application/views/pages/employee_info.php
+++ codeigniter/application/views/pages/employee_info.php
@@ -1,4 +1,4 @@
-<body>
+
 		<div class="container mt-5">
 			<div class="mx-auto">
 				<h1>Employee Information</h1>
@@ -22,15 +22,12 @@
 							</div>
 						</div>
 						<br>
+						<div class="form-row>
 							<div class="col-7">
 								<input class="form-control form-control-lg" type="text" placeholder="Department" name="department" id="department">
 							</div>
+						</div>
+					</div>
 				</form>
 			</div>
 		</div>
-		<!-- Optional JavaScript -->
-		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
-	</body>
