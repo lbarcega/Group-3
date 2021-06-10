b0d4b39eb64c40f9e014059b407e5a3035de45a0
crfirmalo <83999825+crfirmalo@users.noreply.github.com>
2021-06-10 20:17:51 +0800

    Update pending_form.php

--- codeigniter/application/views/pages/pending_form.php
+++ codeigniter/application/views/pages/pending_form.php
@@ -2,47 +2,51 @@
 <h1>Pending Forms</h1>
 <hr>
 <table>
-  <tr>
-    <th>Date Applied</th>
-    <th>Employee Name</th>
-    <th>Start Date</th>
-    <th>End Date</th>
-  </tr>
-  <tr>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-  </tr>
-  <tr>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-  </tr>
-  <tr>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-  </tr>
-  <tr>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-  </tr>
-  <tr>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-  </tr>
-  <tr>
-    <td></td>
-    <td></td>
-    <td></td>
-    <td></td>
-  </tr>
+  <thead>
+	<tr>
+		<th>Date Applied</th>
+		<th>Employee Name</th>
+		<th>Start Date</th>
+		<th>End Date</th>
+	</tr>
+  </thead>
+  <tbody>
+	<tr>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+  </tbody>
 </table>
 </div>
