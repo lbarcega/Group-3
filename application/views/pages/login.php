2e0fbe4359a2beda1cb569dec1f196aefddbe311
komedrano <komedrano@student.apc.edu.ph>
2021-06-17 17:48:29 +0800

    update login.php

--- application/views/pages/login.php
+++ application/views/pages/login.php
@@ -1,19 +1,22 @@
 
     <div class="container mt-5">
-		<div class="mx-auto form-narrow">
+		<div class="mx-auto">
 			<h1>Log In</h1>
 			<hr>
 			<form action="" method="post">
-				<div class="form-group">
+                <center>
+				<div class="form-group col-lg-7">
 
 					<input type="text" class="form-control form-control-lg" placeholder="Username" name="username" required>
 				</div>
-				<div class="form-group">
+				<div class="form-group col-lg-7">
 
 					<input type="password" class="form-control form-control-lg" placeholder="Password" name="password" required>
 				</div>
-					
-				<button type="submit" class="btn btn-block btn-primary submit-button">Log In</button>				
+            
+                <br>
+				<button type="submit" class="btn btn-block btn-primary submit-button">Log In</button>
+                </center>
 			</form>
 	    </div>
     </div>	  
