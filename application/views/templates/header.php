d9b860adcc058ea0a6cbd2fbb91a8e289774ee09
komedrano <komedrano@student.apc.edu.ph>
2021-06-19 17:27:05 +0800

    added arrow in back button

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -7,6 +7,8 @@
 		<!-- Bootstrap CSS -->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.1/css/all.css">
+
 		<!-- Custom CSS -->
 		<link rel="stylesheet" type="text/css" href="../assets/css/style.css">
                 <title>Time Off <?php echo $title; ?></title>
@@ -17,4 +19,4 @@
 		<div class="bg">
 			<img src="../assets/images/logo.png" alt="logo">
 		</div>
-        <button class = "btn btn-secondary back-button mt-3 ml-3" onclick="history.go(-1);">Back </button>
+        <button class = "btn btn-secondary back-button mt-3 ml-3" onclick="history.go(-1);"><i class="fas fa-arrow-left"></button>
