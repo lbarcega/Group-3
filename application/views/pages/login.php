2186f1539e582d8ef28c3e90c31295f59f1b44c1
lbarcega <84000664+lbarcega@users.noreply.github.com>
2021-06-20 12:59:08 +0800

    Update login.php

--- application/views/pages/login.php
+++ application/views/pages/login.php
@@ -3,15 +3,15 @@
 		<div class="mx-auto">
 			<h1>Log In</h1>
 			<hr>
-			<form action="" method="post">
+			<form action="<?php echo site_url(); ?>/login/validation" method="post">
                 <center>
 				<div class="form-group col-lg-7">
 
-					<input type="text" class="form-control form-control-lg" placeholder="Username" name="username" required>
+					<input type="text" class="form-control form-control-lg" placeholder="Username" name="username" value="<?php echo set_value('email'); ?> required>
 				</div>
 				<div class="form-group col-lg-7">
 
-					<input type="password" class="form-control form-control-lg" placeholder="Password" name="password" required>
+					<input type="password" class="form-control form-control-lg" placeholder="Password" name="password" value="<?php echo set_value('password'); ?>"  required>
 				</div>
             
                 <br>
