dda4335096e4feeb68df2073004ae0d737014de2
lbarcega <84000664+lbarcega@users.noreply.github.com>
2021-06-20 12:32:58 +0800

    Update home.php
    change links

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -2,18 +2,18 @@
 			<div class="mx-auto">
 				<h1>Employee Home</h1>
 				<hr>
-						<a href="index.php/employee_info"  class="btn btn-primary btn-lg btn-block home-button">Employee Information</a>
+						<a href="<?php echo site_url(); ?>/employee_info"  class="btn btn-primary btn-lg btn-block home-button">Employee Information</a>
 						<br>
 					<div class="row">
 						<div class="col">
-								<a href="index.php/login" class="btn btn-primary btn-lg btn-block home-button">Sign In</a>
+								<a href="#" class="btn btn-primary btn-lg btn-block home-button">Sign In</a>
 						</div>
 							<div class="col">
-								<a href="" class="btn btn-primary btn-lg btn-block home-button">Sign Out</a>
+								<a href="<?php echo site_url(); ?>/pages/logout" class="btn btn-primary btn-lg btn-block home-button">Sign Out</a>
 							</div>
 						</div>
 						<br>
-							<a href="index.php/leave_form" class="btn btn-primary btn-lg btn-block home-button">Apply for Leave</a>
+							<a href="<?php echo site_url(); ?>/leave_form" class="btn btn-primary btn-lg btn-block home-button">Apply for Leave</a>
 
 			</div>
 		</div>
