7b0155a8d3de736fa034e29083cac6b748d48fb9
lbarcega <84000664+lbarcega@users.noreply.github.com>
2021-06-20 19:23:15 +0800

    Create Logout.php
    create logout function

---
+++ application/controllers/Logout.php
@@ -0,0 +1,11 @@
+<?php
+function logout()
+	{
+		$data = $this->session->all_userdata();
+		foreach($data as $row => $rows_value)
+		{
+			$this->session->unset_userdata($row);
+		}
+		redirect('login');
+	}
+?>
